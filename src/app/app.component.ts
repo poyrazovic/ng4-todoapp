570db717d77d29fe6ee2a1b8ef72051adaffa742
poyrazovic <sertacpoyrazz@gmail.com>
2018-04-10 12:16:48 +0300

    used localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   inputErrorStatus = false;
   name = '';
-  id = 1;
+  id: number = 1;
   items = [];
 
   addItem() {
     if (this.name !== '') {
       this.inputErrorStatus = false;
-      this.id++;
+      let todo = this.getTodoItem();
+      if(todo.length == 0) {
+        this.id = 1;
+      } else {
+        let maxId = Number(todo[todo.length - 1].id);
+        this.id = maxId + 1;
+      }
       this.items.push({id: `${this.id}`, name: `${this.name}`});
       this.name = '';
+      this.setItem();
+      this.getTodoItem();
+      this.id++;
     } else {
       this.inputErrorStatus = true;
     }
   }
 
   removeItem(data) {
-    this.items = this.items.filter(item => item.id !== data);
+    this.items = JSON.parse(localStorage.getItem('todos')).filter(item => item.id !== data);
+    this.setItem();
+  }
+
+  getTodoItem() {
+    if (localStorage.getItem('todos') == null || localStorage.getItem('todos') == '') {
+      localStorage.todos = [];
+    } else {
+      this.items = JSON.parse(localStorage.getItem('todos'));
+      return JSON.parse(localStorage.getItem('todos'));
+    }
+  }
+
+  setItem() {
+    localStorage.setItem('todos', JSON.stringify(this.items));
+  }
+
+  ngOnInit() {
+    this.getTodoItem();
   }
 }
